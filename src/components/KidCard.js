48dbe4b2ff6aa8c389a4ae1749e0ebf194935391
Nicolle Romero <nicollearomero@gmail.com>
2020-10-19 16:00:43 -0700

    Updated to functional components

--- src/components/KidCard.js
+++ src/components/KidCard.js
@@ -4,51 +4,49 @@ import { Col } from 'react-bootstrap';
 import { ChoreList } from './ChoreList';
 import { GoalLists } from './GoalLists';
 
-export class KidCard extends React.Component {
+export function KidCard(props) {
 
-  calculatePointsTotal() {
-    return this.props.choreList
+  function calculatePointsTotal() {
+    return props.choreList
       .filter((chore) => chore.complete)
       .reduce((sum, chore) => sum + Number(chore.points), 0);
   }
 
-  render() {
-    let completedGoalsList = [];
-    let incompleteGoalsList = [];
+  let completedGoalsList = [];
+  let incompleteGoalsList = [];
 
-    let remainingPoints = this.calculatePointsTotal();
-    let incomplete = false;
+  let remainingPoints = calculatePointsTotal();
+  let incomplete = false;
 
-    for (let goal of this.props.goalList) {
-      if (!incomplete && goal.points && remainingPoints >= goal.points) {
-        completedGoalsList.unshift(goal);
-        remainingPoints = remainingPoints - goal.points;
-      } else {
-        incompleteGoalsList.push(goal);
-        incomplete = true;
-      }
+  for (let goal of props.goalList) {
+    if (!incomplete && goal.points && remainingPoints >= goal.points) {
+      completedGoalsList.unshift(goal);
+      remainingPoints = remainingPoints - goal.points;
+    } else {
+      incompleteGoalsList.push(goal);
+      incomplete = true;
     }
-
-    return (
-      <Col xs={6}>
-        <ChoreList
-          assignee={this.props.assignee}
-          choreList={this.props.choreList}
-          onDelete={this.props.onDelete}
-          onChange={this.props.onChange}
-        />
-
-        <GoalLists
-          assignee={this.props.assignee}
-          completedPoints={remainingPoints}
-          completedGoalsList={completedGoalsList}
-          incompleteGoalsList={incompleteGoalsList}
-          onAddGoal={this.props.onAddGoal}
-          onDeleteGoal={this.props.onDeleteGoal}
-          onChangeGoal={this.props.onChangeGoal}
-          onGoalCompleted={this.props.onGoalCompleted}
-        />
-      </Col>
-    );
   }
+
+  return (
+    <Col xs={6}>
+      <ChoreList
+        assignee={props.assignee}
+        choreList={props.choreList}
+        onDelete={props.onDelete}
+        onChange={props.onChange}
+      />
+
+      <GoalLists
+        assignee={props.assignee}
+        completedPoints={remainingPoints}
+        completedGoalsList={completedGoalsList}
+        incompleteGoalsList={incompleteGoalsList}
+        onAddGoal={props.onAddGoal}
+        onDeleteGoal={props.onDeleteGoal}
+        onChangeGoal={props.onChangeGoal}
+        onGoalCompleted={props.onGoalCompleted}
+      />
+    </Col>
+  );
 }
--- src/components/SetupModal.js
+++ src/components/SetupModal.js
@@ -7,148 +7,141 @@ import { DeleteButton } from './DeleteButton';
 import './SetupModal.css';
 
 
-export class SetupModal extends React.Component {
-  state = {
-    inputValue: '',
-    editing: false,
-  }
+export function SetupModal(props) {
+  const [inputValue, setInputValue] = React.useState('');
+  const [editing, setEditing] = React.useState(false);
 
-  addAssignee() {
+  function addAssignee() {
     const newAssignee = {
       id: Math.random().toString(36).slice(2),
-      name: this.state.inputValue.trim(),
+      name: inputValue.trim(),
     }
 
-    this.props.onAddAssignee(newAssignee);
+    props.onAddAssignee(newAssignee);
 
-    this.setState({
-      inputValue: '',
-    });
+    setInputValue('');
   }
 
-  updateInput(value) {
-    this.setState({
-      inputValue: value.trimStart(),
-    });
+  function updateInput(value) {
+    setInputValue(value.trimStart());
   }
 
-  updateBoardName(value) {
-    this.props.onChangeBoardName(value.trimStart());
+  function updateBoardName(value) {
+    props.onChangeBoardName(value.trimStart());
   }
 
-  handleFocus = () => {
-    this.setState({ editing: true });
+  function handleFocus() {
+    setEditing(!editing);
   }
 
-  handleBlur = (assignee) => {
-    this.setState({ editing: false });
+  function handleBlur(assignee) {
+    setEditing(!editing);
 
     if (!assignee.name) {
-      this.props.onDelete(assignee.id);
+      props.onDelete(assignee.id);
     }
   }
 
-  render() {
-    return (
-      <Modal
-        show={this.props.show}
-        onHide={this.props.onHide}
-      >
-        <Modal.Header className="align-items-center">
-          <h4 class="chore-maintitle text-center align-items-center">👨‍👩‍👧‍👦 Update Board Name</h4>
-        </Modal.Header>
-        <div class="card-body text-center">
-          <Modal.Body>
-            <Form onSubmit={(e) => e.preventDefault()}>
-              <Form.Row controlId="formBasicEmail">
-                <InputGroup className="align-items-center">
-                  <Form.Control
-                    class="appearance-none bg-transparent border-none w-full text-center text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                    type="text"
-                    value={this.props.boardName}
-                    onChange={(e) => this.updateBoardName(e.target.value)}
-                    placeholder="Enter name for board"
-                  />
-                </InputGroup>
-              </Form.Row>
-            </Form>
-          </Modal.Body>
-        </div>
+  return (
+    <Modal
+      show={props.show}
+      onHide={props.onHide}
+    >
+      <Modal.Header className="align-items-center align-self-center">
+        <h4 className="chore-maintitle text-center align-items-center text-center">Update Board Name</h4>
+      </Modal.Header>
+      <div className="card-body text-center">
+        <Modal.Body>
+          <Form onSubmit={(e) => e.preventDefault()}>
+            <Form.Row controlId="formBasicEmail">
+              <InputGroup className="align-items-center">
+                <Form.Control
+                  className="appearance-none bg-transparent border-none w-full text-center text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                  type="text"
+                  value={props.boardName}
+                  onChange={(e) => updateBoardName(e.target.value)}
+                  placeholder="Enter name for board"
+                />
+              </InputGroup>
+            </Form.Row>
+          </Form>
+        </Modal.Body>
+      </div>
 
-        <Modal.Header className="align-items-center">
-          <h4 class="chore-maintitle text-center align-items-center">😀 Update Assignees</h4>
-        </Modal.Header>
-        <div class="card-body text-center">
-          <Modal.Body>
-            <Form onSubmit={(e) => e.preventDefault()}>
-              <Form.Row controlId="formBasicEmail">
-                <InputGroup className="align-items-center">
-                  <Form.Control
-                    class="appearance-none bg-transparent border-none w-full text-center text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                    type="text"
-                    value={this.state.inputValue}
-                    onChange={(e) => this.updateInput(e.target.value)}
-                    placeholder="Enter new assignee's name 👋"
-                  />
-                  <Button
-                    disabled={!this.state.inputValue}
-                    type="submit"
-                    class="btn btn-primary"
-                    onClick={() => this.addAssignee()}
-                  >
-                    +
+      <Modal.Header className="align-items-center align-self-center">
+        <h4 className="chore-maintitle text-center align-items-center">Update Assignees</h4>
+      </Modal.Header>
+      <div className="card-body text-center">
+        <Modal.Body>
+          <Form onSubmit={(e) => e.preventDefault()}>
+            <Form.Row controlId="formBasicEmail">
+              <InputGroup className="align-items-center" variant="flush">
+                <Form.Control
+                  className="appearance-none bg-transparent border-none w-full text-center text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                  type="text"
+                  value={inputValue}
+                  onChange={(e) => updateInput(e.target.value)}
+                  placeholder="Enter new assignee's name 👋"
+                />
+                <Button
+                  disabled={!inputValue}
+                  type="submit"
+                  className="btn btn-primary"
+                  onClick={() => addAssignee()}
+                >
+                  +
                   </Button>
-                </InputGroup>
-              </Form.Row>
-            </Form>
+              </InputGroup>
+            </Form.Row>
+          </Form>
 
-            <div class="card-body text-center">
-              <ListGroup variant="flush">
-                <div className="align-text-center current-assignees">
-                  Current Assignees
+          <div className="card-body text-center">
+            <ListGroup variant="flush">
+              <div className="align-text-center current-assignees">
+                Current Assignees
               </div>
-                <FlipMove duration={350} easing="ease-out">
-                  {this.props.assignees.map((assignee) => {
-                    return (
-                      <form
-                        class="w-full max-w-lg"
-                        onSubmit={(e) => e.preventDefault()}
-                      >
-                        <div class="flex md:items-center border-b border-b-2 border-blue-500 py-2">
-                          <input
-                            class="text-center bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                            type="text"
-                            value={assignee.name}
-                            onChange={(e) => this.props.onChange(assignee.id, 'name', e.target.value)}
-                            onFocus={this.handleFocus}
-                            onBlur={() => this.handleBlur(assignee)}
+              <FlipMove duration={350} easing="ease-out">
+                {props.assignees.map((assignee) => {
+                  return (
+                    <form
+                      key={assignee.id}
+                      className="w-full max-w-lg"
+                      onSubmit={(e) => e.preventDefault()}
+                    >
+                      <div className="flex md:items-center border-b border-b-2 border-blue-500 py-2">
+                        <input
+                          className="text-center bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                          type="text"
+                          value={assignee.name}
+                          onChange={(e) => props.onChange(assignee.id, 'name', e.target.value)}
+                          onFocus={handleFocus}
+                          onBlur={() => handleBlur(assignee)}
+                        />
+                        {editing && (
+                          <DeleteButton
+                            onClick={() => props.onDelete(assignee.id)}
                           />
-                          {this.state.editing && (
-                            <DeleteButton
-                              onClick={() => this.props.onDelete(assignee.id)}
-                            />
-                          )}
-                        </div>
-                      </form>
-                    );
-                  })}
-                </FlipMove>
-              </ListGroup>
-            </div>
-          </Modal.Body>
-        </div>
-        <Modal.Footer>
-          <Button
-            type="submit"
-            class="btn btn-secondary"
-            variant="secondary"
-            disabled={!this.props.assignees.length}
-            onClick={this.props.onHide}
-          >
-            Close
+                        )}
+                      </div>
+                    </form>
+                  );
+                })}
+              </FlipMove>
+            </ListGroup>
+          </div>
+        </Modal.Body>
+      </div>
+      <Modal.Footer>
+        <Button
+          type="submit"
+          className="btn btn-secondary"
+          variant="secondary"
+          disabled={!props.assignees.length}
+          onClick={props.onHide}
+        >
+          Close
           </Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
+      </Modal.Footer>
+    </Modal>
+  );
 }
