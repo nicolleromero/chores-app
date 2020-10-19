f2ed597573b3cf8bf54b1f70e0f0f9596f16afeb
Nicolle Romero <nicollearomero@gmail.com>
2020-10-19 16:00:13 -0700

    Updated to functional components

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -3,17 +3,15 @@ import React from 'react';
 
 import './DeleteButton.css';
 
-export class DeleteButton extends React.Component {
+export function DeleteButton(props) {
 
-  render() {
-    return (
-      <button
-        className="btn btn-sm delete-button"
-        onClick={this.props.onClick}
-        onMouseDown={(e) => e.preventDefault()}
-      >
-        ❌
-      </button>
-    );
-  }
+  return (
+    <button
+      className="btn btn-sm delete-button"
+      onClick={props.onClick}
+      onMouseDown={(e) => e.preventDefault()}
+    >
+      ❌
+    </button>
+  );
 }
--- src/components/DropdownSelect.js
+++ src/components/DropdownSelect.js
@@ -14,34 +14,32 @@ const styles = {
   },
 };
 
-class InnerDropdownSelect extends React.Component {
+function InnerDropdownSelect(props) {
 
-  render() {
-    let currentAssignee = this.props.assignees.find((assignee) => {
-      return assignee.id === this.props.selected;
-    })
+  let currentAssignee = props.assignees.find((assignee) => {
+    return assignee.id === props.selected;
+  })
 
-    return (
-      <TextField
-        className={this.props.classes.root}
-        id="outlined-select-assignee"
-        select
-        label="Assignee"
-        title={currentAssignee ? currentAssignee.name : 'Assignee'}
-        value={this.props.selected}
-        onChange={(e) => this.props.onSelect(e.target.value)}
-        variant="outlined"
-      >
-        {this.props.assignees.map((assignee) => {
-          return (
-            <MenuItem key={assignee.id} value={assignee.id}>
-              {assignee.name}
-            </MenuItem>
-          )
-        })}
-      </TextField>
-    );
-  }
+  return (
+    <TextField
+      className={props.classes.root}
+      id="outlined-select-assignee"
+      select
+      label="Assignee"
+      title={currentAssignee ? currentAssignee.name : 'Assignee'}
+      value={props.selected}
+      onChange={(e) => props.onSelect(e.target.value)}
+      variant="outlined"
+    >
+      {props.assignees.map((assignee) => {
+        return (
+          <MenuItem key={assignee.id} value={assignee.id}>
+            {assignee.name}
+          </MenuItem>
+        )
+      })}
+    </TextField>
+  );
 }
 
 export const DropdownSelect = withStyles(styles)(InnerDropdownSelect);
